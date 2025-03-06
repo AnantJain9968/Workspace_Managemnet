13163c8ca1ff2b10cfe371123430faf08fe37b3c
Anant Jain <anant.jain@st.com>
2025-03-06 16:22:44 +0530

    fixing entity

--- workspace.management/src/main/java/com/st/workspace/management/entity/SeatAllocation.java
+++ workspace.management/src/main/java/com/st/workspace/management/entity/SeatAllocation.java
@@ -1,5 +1,30 @@
 package com.st.workspace.management.entity;
 
-public class SeatAllocation {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "spam_seat_allocation")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SeatAllocation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long allocationId;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_id")
+    private Employee employee;
+
+    @ManyToOne
+    @JoinColumn(name = "seat_id")
+    private Seat seat;
+
+    private Date allocationDate;
+    private String status; // Allocated, Vacant, Reserved
 }
