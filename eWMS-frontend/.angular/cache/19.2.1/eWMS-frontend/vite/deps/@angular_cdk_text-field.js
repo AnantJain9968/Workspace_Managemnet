0d1b434ef3cd6659c819b774cf9c491928b08b52
Prankur Jain <jn.prankur.jain@gmail.com>
2025-03-06 15:37:08 +0530

    angular change

---
+++ eWMS-frontend/.angular/cache/19.2.1/eWMS-frontend/vite/deps/@angular_cdk_text-field.js
@@ -0,0 +1,16 @@
+import {
+  AutofillMonitor,
+  CdkAutofill,
+  CdkTextareaAutosize,
+  TextFieldModule
+} from "./chunk-GH76HGVM.js";
+import "./chunk-TFECLKXS.js";
+import "./chunk-JZ7LKULK.js";
+import "./chunk-ZUXQ7BAO.js";
+import "./chunk-77B6OTTQ.js";
+export {
+  AutofillMonitor,
+  CdkAutofill,
+  CdkTextareaAutosize,
+  TextFieldModule
+};
---
+++ eWMS-frontend/src/main.ts
@@ -0,0 +1,23 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter, Routes } from '@angular/router';
+import { AppComponent } from './app/app.component';
+import { LoginComponent } from './app/login/login.component';
+import { HomeComponent } from './app/home/home.component';
+import { BuildingComponent } from './app/building/building.component';
+import { FloorComponent } from './app/floor/floor.component';
+import { FloorMapComponent } from './app/floor-map/floor-map.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'building/:site', component: BuildingComponent },
+  { path: 'floor/:building', component: FloorComponent },
+  { path: 'floor-map/:floor', component: FloorMapComponent },
+];
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes)
+  ]
+}).catch(err => console.error(err));
