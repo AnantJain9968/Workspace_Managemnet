317f4db182229969b444138ca7b05d1de718cc48
Suyash Kejriwal <suyashkejriwal@st.com>
2025-03-07 15:55:00 +0530

    frontend change

--- eWMS-frontend/src/app/home/home.component.ts
+++ eWMS-frontend/src/app/home/home.component.ts
@@ -62,6 +62,10 @@ export class HomeComponent implements OnInit {
       this.offices = data;
       this.filteredOffices = [...this.offices];
       console.log('Sites:', this.offices);
+
+      for(let office of this.offices){
+        office.image=''
+      }
     });
     
   }
--- eWMS-frontend/src/app/view-building/view-building.component.ts
+++ eWMS-frontend/src/app/view-building/view-building.component.ts
@@ -3,7 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
-
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/env';
 @Component({
   selector: 'app-view-building',
   imports: [MatCardModule,CommonModule],
@@ -11,19 +12,23 @@ import { CommonModule } from '@angular/common';
   styleUrl: './view-building.component.css'
 })
 export class ViewBuildingComponent implements OnInit{
-site: string | null = null;
-  buildings = [
-    { name: 'Building 1', image: 'assets/building1.jpg' },
-    { name: 'Building 2', image: 'assets/building2.jpg' },
-    { name: 'Building 3', image: 'assets/building3.jpg' },
-    { name: 'Building 4', image: 'assets/building4.jpg' }
-  ];
+siteId!: any;
+  buildings! : any; 
 
-  constructor(private route: ActivatedRoute, private router: Router) { }
+  constructor(private route: ActivatedRoute, private router: Router,private http: HttpClient) { }
 
   ngOnInit(): void {
-     this.site = this.route.snapshot.paramMap.get('siteId');
-     console.log("Site ID from View building::", this.site);
+     this.siteId = this.route.snapshot.paramMap.get('siteId');
+     console.log("Site ID from View building::", this.siteId);
+
+    
+     
+         this.http.get(`${environment.apiUrl}/api/sites/${this.siteId}`).subscribe((data: any) => {
+           
+           console.log('Sites in view building:', data);
+
+           this.buildings=data.buildings;
+         });
   }
 
   navigateToFloor(building: string): void {
