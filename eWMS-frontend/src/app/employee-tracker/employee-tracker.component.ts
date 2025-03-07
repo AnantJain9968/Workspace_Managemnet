61eb1da09406b552fae84bdcfdd82eb03fdad781
Prankur Jain <jn.prankur.jain@gmail.com>
2025-03-07 15:54:42 +0530

    chads

--- eWMS-frontend/src/app/employee-tracker/employee-tracker.component.ts
+++ eWMS-frontend/src/app/employee-tracker/employee-tracker.component.ts
@@ -6,16 +6,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { environment } from '../../environments/env'; 
 import { CommonModule } from '@angular/common';
 
+import { ToastrService } from 'ngx-toastr';
+
+
 import { MatSelectModule } from '@angular/material/select';
 import { MatOptionModule } from '@angular/material/core';
+import { catchError, tap, throwError } from 'rxjs';
 @Component({
   selector: 'app-employee-tracker',
   standalone: true,
-  imports: [ReactiveFormsModule,MatFormFieldModule,MatInputModule,MatButtonModule,MatOptionModule,CommonModule,MatSelectModule],
+  imports: [ ReactiveFormsModule,MatFormFieldModule,MatInputModule,MatButtonModule,MatOptionModule,CommonModule,MatSelectModule],
   templateUrl: './employee-tracker.component.html',
   styleUrl: './employee-tracker.component.css'
 })
@@ -32,16 +36,16 @@ export class EmployeeTrackerComponent implements OnInit {
     'Engineering': ['Development', 'QA'],
     'Marketing': ['SEO', 'Content']
   };
-  constructor(private fb: FormBuilder,private http: HttpClient) { }
+  constructor(private fb: FormBuilder,private http: HttpClient,private toastr: ToastrService) { }
 
   ngOnInit(): void {
 
     this.getDeptJGData();
     this.employeeForm = this.fb.group({
       employeeName: ['',Validators.required],
-      department: ['',Validators.required],
-      subDepartment: ['',Validators.required],
-      jobGrade: ['',Validators.required]
+      departmentId: ['',Validators.required],
+      subDepartmentId: ['',Validators.required],
+      jobGradeName: ['',Validators.required]
     });
   }
 
@@ -75,18 +79,34 @@ export class EmployeeTrackerComponent implements OnInit {
       this.subDepartments = [];
     }
     // Reset the subDepartment control value
-    this.employeeForm.get('subDepartment')!.setValue('');
+    this.employeeForm.get('subDepartmentId')!.setValue('');
   }
 
   addEmployee(): void {
     if (this.employeeForm.valid) {
-      this.http.post(`${environment.apiUrl}/api/seat-allocation/allocateEmployee`, this.employeeForm.value).subscribe(response => {
-        console.log('Employee added successfully', response);
-        // Handle success response
-      }, error => {
-        console.error('Error adding employee', error);
-        // Handle error response
-      });
+      this.http.post(`${environment.apiUrl}/api/seat-allocation/allocateEmployee`, this.employeeForm.value, { responseType: 'text' })
+        .pipe(
+          tap(response => {
+            console.log('Employee added successfully', response);
+            this.toastr.success('Success!', 'Employee Added successfully!');
+            // Handle success response
+          }),
+          catchError((error: HttpErrorResponse) => {
+            console.error('Error adding employee', error);
+            if (error.error instanceof ErrorEvent) {
+              // Client-side error
+              console.error('Client-side error:', error.error.message);
+            } else {
+              // Server-side error
+              console.error(`Server-side error: ${error.status} - ${error.message}`);
+              console.error('Response body:', error.error);
+            }
+            // Handle error response
+            this.toastr.error('Error!', 'Failed to add employee.');
+            return throwError(error);
+          })
+        )
+        .subscribe();
     }
   }
 
@@ -95,6 +115,7 @@ export class EmployeeTrackerComponent implements OnInit {
     if (employeeId) {
       this.http.delete(`${environment.apiUrl}/api/employee/delete/${employeeId}`).subscribe(response => {
         console.log('Employee deleted successfully', response);
+        this.toastr.success('Success!', 'Employee deleted successfully!');
         // Handle success response
       }, error => {
         console.error('Error deleting employee', error);
